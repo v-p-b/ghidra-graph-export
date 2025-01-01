5568b0942b14354aaa2cffee524be4d2236fcfd7
b <vpbalint@silentsignal.hu>
2025-01-01 21:45:22 +0100

    Graphology compatible JSON output

--- graph_export.py
+++ graph_export.py
@@ -3,6 +3,7 @@
 
 # based on code by cetfor: https://github.com/NationalSecurityAgency/ghidra/issues/855#issuecomment-569355675
 
+import json
 import tempfile
 import os
 
@@ -10,19 +11,70 @@ from ghidra.program.model.block import BasicBlockModel
 from ghidra.util.task import ConsoleTaskMonitor
 
 
+def collect_nodes(elist):
+    ret = set()
+    for f, t in elist:
+        ret.add(f)
+        ret.add(t)
+    return ret
+
+
+def address2name(addr):
+    return "BB%s" % (str(addr))
+
+
+def block_instructions_count(block):
+    ret = 0
+    for a in block.getAddresses(True):
+        if getInstructionAt(a) != None:
+            ret += 1
+    return ret
+
+
 def export_dot(name, elist, path):
-    outName = "%s.dot" % (name)
-    outPath = os.path.join(path, outName)
-    with open(outPath, "w") as out:
+    out_name = "%s.dot" % (name)
+    out_path = os.path.join(path, out_name)
+    with open(out_path, "w") as out:
         out.write("digraph %s {\n" % (name))
         out.write('  node [shape="box"];\n')
         out.write("  graph [splines=ortho];\n")
         for e in elist:
-            f = str(e[0].getFirstStartAddress())
-            t = str(e[1].getFirstStartAddress())
-            out.write("  BB%s -> BB%s;\n" % (f, t))
+            f = e[0].getFirstStartAddress()
+            t = e[1].getFirstStartAddress()
+            out.write("  %s -> %s;\n" % (address2name(f), address2name(t)))
         out.write("}\n")
-    print("Exported %s" % (outPath))
+    print("Exported %s" % (out_path))
+
+
+def export_json(name, elist, path):
+    out_name = "%s.json" % (name)
+    out_path = os.path.join(path, out_name)
+    data = {}
+    data["options"] = {"type": "directed", "multi": True, "allowSelfLoops": True}
+    data["attributes"] = {}
+    data["nodes"] = []
+    data["edges"] = []
+    nodes = collect_nodes(elist)
+
+    for n in nodes:
+        node_data = {}
+        node_data["key"] = address2name(n.getFirstStartAddress())
+        node_data["attributes"] = {}
+        node_data["attributes"]["lines"] = block_instructions_count(n)
+        data["nodes"].append(node_data)
+
+    for s, t in elist:
+        edge_data = {}
+        edge_data["source"] = address2name(s.getFirstStartAddress())
+        edge_data["target"] = address2name(t.getFirstStartAddress())
+        edge_data["attributes"] = {}
+        edge_data["attributes"]["type"] = "regular"  # TODO
+        data["edges"].append(edge_data)
+
+    with open(out_path, "w") as out:
+        json.dump(data, out)
+
+    print("Exported %s" % (out_path))
 
 
 edge_list = []
@@ -46,3 +98,4 @@ while blocks.hasNext():
             edge_list.append((dbb.getSourceBlock(), dbb.getDestinationBlock()))
 
 export_dot(funcName, edge_list, tmpDir)
+export_json(funcName, edge_list, tmpDir)
